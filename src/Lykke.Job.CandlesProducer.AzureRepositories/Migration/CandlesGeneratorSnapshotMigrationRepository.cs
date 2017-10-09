adbad2fa56490163bc40983f259d4eb6e1554d79
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-10-10 01:39:20 +0300

    Legacy snapshot reading logging was added

--- src/Lykke.Job.CandlesProducer.AzureRepositories/Migration/CandlesGeneratorSnapshotMigrationRepository.cs
+++ src/Lykke.Job.CandlesProducer.AzureRepositories/Migration/CandlesGeneratorSnapshotMigrationRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Immutable;
 using System.Threading.Tasks;
 using AzureStorage;
+using Common.Log;
 using Lykke.Job.CandlesProducer.AzureRepositories.Legacy;
 using Lykke.Job.CandlesProducer.Core.Domain;
 using Lykke.Job.CandlesProducer.Core.Domain.Candles;
@@ -9,11 +10,13 @@ namespace Lykke.Job.CandlesProducer.AzureRepositories.Migration
 {
     public class CandlesGeneratorSnapshotMigrationRepository : ISnapshotRepository<IImmutableDictionary<string, ICandle>>
     {
+        private readonly ILog _log;
         private readonly LegacyCandlesGeneratorSnapshotRepository _legacyRepository;
         private readonly CandlesGeneratorSnapshotRepository _repository;
 
-        public CandlesGeneratorSnapshotMigrationRepository(IBlobStorage storage)
+        public CandlesGeneratorSnapshotMigrationRepository(IBlobStorage storage, ILog log)
         {
+            _log = log;
             _legacyRepository = new LegacyCandlesGeneratorSnapshotRepository(storage);
             _repository = new CandlesGeneratorSnapshotRepository(storage);
         }
@@ -25,7 +28,16 @@ namespace Lykke.Job.CandlesProducer.AzureRepositories.Migration
 
         public async Task<IImmutableDictionary<string, ICandle>> TryGetAsync()
         {
-            return await _repository.TryGetAsync() ?? await _legacyRepository.TryGetAsync();
+            var newResult = await _repository.TryGetAsync();
+            if (newResult == null)
+            {
+                await _log.WriteWarningAsync(nameof(CandlesGeneratorSnapshotMigrationRepository), nameof(TryGetAsync), "",
+                    "Failed to get snapshot in the new format, fallback to the legacy format");
+
+                return await _legacyRepository.TryGetAsync();
+            }
+
+            return newResult;
         }
     }
 }
