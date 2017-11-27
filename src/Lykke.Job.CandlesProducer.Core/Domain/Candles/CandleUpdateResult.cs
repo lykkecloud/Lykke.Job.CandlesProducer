33cdcbe41b6a239f2985b9038656255715d36bb4
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-11-28 00:57:28 +0300

    LWDEV-3989 No candle is returned in case of cache miss, since we can't produce full state

--- src/Lykke.Job.CandlesProducer.Core/Domain/Candles/CandleUpdateResult.cs
+++ src/Lykke.Job.CandlesProducer.Core/Domain/Candles/CandleUpdateResult.cs
@@ -2,10 +2,12 @@
 {
     public class CandleUpdateResult
     {
+        public static readonly CandleUpdateResult Empty = new CandleUpdateResult(null, null, false);
+
         public Candle Candle { get; }
         public Candle OldCandle { get; }
         public bool WasChanged { get; }
-
+        
         public CandleUpdateResult(Candle candle, Candle oldCandle, bool wasChanged)
         {
             Candle = candle;
