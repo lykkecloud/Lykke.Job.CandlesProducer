d0c2488c216a89aa1a3dc44badded67048a19649
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2018-02-19 23:51:02 +0300

    LWDEV-5848 v1 candles contract has been removed

--- src/Lykke.Job.CandlesProducer.Services/Candles/CandlesPublisher.cs
+++ src/Lykke.Job.CandlesProducer.Services/Candles/CandlesPublisher.cs
@@ -7,7 +7,6 @@ using Lykke.Job.CandlesProducer.Contract;
 using Lykke.Job.CandlesProducer.Core.Domain.Candles;
 using Lykke.Job.CandlesProducer.Core.Services;
 using Lykke.Job.CandlesProducer.Core.Services.Candles;
-using Lykke.Job.CandlesProducer.Services.Candles.LegacyContract;
 using Lykke.RabbitMqBroker.Publisher;
 
 namespace Lykke.Job.CandlesProducer.Services.Candles
@@ -18,7 +17,6 @@ namespace Lykke.Job.CandlesProducer.Services.Candles
         private readonly IRabbitMqPublishersFactory _publishersFactory;
         private readonly IRabbitPublicationSettings _settings;
 
-        private RabbitMqPublisher<CandleMessageV1> _legacyPublisher;
         private RabbitMqPublisher<CandlesUpdatedEvent> _publisher;
 
         public CandlesPublisher(IRabbitMqPublishersFactory publishersFactory, IRabbitPublicationSettings settings)
@@ -29,12 +27,6 @@ namespace Lykke.Job.CandlesProducer.Services.Candles
 
         public void Start()
         {
-            _legacyPublisher = _publishersFactory.Create(
-                new JsonMessageSerializer<CandleMessageV1>(),
-                _settings.ConnectionString,
-                _settings.Namespace,
-                "candles");
-
             _publisher = _publishersFactory.Create(
                 new MessagePackMessageSerializer<CandlesUpdatedEvent>(),
                 _settings.ConnectionString,
@@ -44,78 +36,51 @@ namespace Lykke.Job.CandlesProducer.Services.Candles
 
         public Task PublishAsync(IReadOnlyCollection<CandleUpdateResult> updates)
         {
-            return Task.WhenAll(PublishV2Async(updates), PublishV1Async(updates));
-        }
-
-        private Task PublishV1Async(IEnumerable<CandleUpdateResult> updates)
-        {
-            lock (_legacyPublisher)
-            {
-                foreach (var candle in updates.Select(c => c.Candle))
-                {
-                    // HACK: Actually ProduceAsync is not async, so not need to await it and lock works well
-
-                    _legacyPublisher.ProduceAsync(new CandleMessageV1
-                    {
-                        AssetPairId = candle.AssetPairId,
-                        PriceType = candle.PriceType,
-                        TimeInterval = candle.TimeInterval,
-                        Timestamp = candle.Timestamp,
-                        Open = candle.Open,
-                        Close = candle.Close,
-                        Low = candle.Low,
-                        High = candle.High,
-                        TradingVolume = candle.TradingVolume,
-                        LastUpdateTimestamp = candle.LatestChangeTimestamp
-                    });
-                }
-            }
-
-            return Task.CompletedTask;
+            return PublishV2Async(updates);
         }
 
         private Task PublishV2Async(IEnumerable<CandleUpdateResult> updates)
         {
+            var @event = new CandlesUpdatedEvent
+            {
+                ContractVersion = Contract.Constants.ContractVersion,
+                UpdateTimestamp = DateTime.UtcNow,
+                Candles = updates
+                    .Select(c => new CandleUpdate
+                    {
+                        IsLatestCandle = c.IsLatestCandle,
+                        IsLatestChange = c.IsLatestChange,
+                        ChangeTimestamp = c.Candle.LatestChangeTimestamp,
+                        AssetPairId = c.Candle.AssetPairId,
+                        PriceType = c.Candle.PriceType,
+                        TimeInterval = c.Candle.TimeInterval,
+                        CandleTimestamp = c.Candle.Timestamp,
+                        Open = c.Candle.Open,
+                        Close = c.Candle.Close,
+                        Low = c.Candle.Low,
+                        High = c.Candle.High,
+                        TradingVolume = c.Candle.TradingVolume,
+                        TradingOppositeVolume = c.Candle.TradingOppositeVolume,
+                        LastTradePrice = c.Candle.LastTradePrice
+                    })
+                    .ToArray()
+            };
+
             lock (_publisher)
             {
                 // HACK: Actually ProduceAsync is not async, so lock works well
 
-                return _publisher.ProduceAsync(new CandlesUpdatedEvent
-                {
-                    ContractVersion = Contract.Constants.ContractVersion,
-                    UpdateTimestamp = DateTime.UtcNow,
-                    Candles = updates
-                        .Select(c => new CandleUpdate
-                        {
-                            IsLatestCandle = c.IsLatestCandle,
-                            IsLatestChange = c.IsLatestChange,
-                            ChangeTimestamp = c.Candle.LatestChangeTimestamp,
-                            AssetPairId = c.Candle.AssetPairId,
-                            PriceType = c.Candle.PriceType,
-                            TimeInterval = c.Candle.TimeInterval,
-                            CandleTimestamp = c.Candle.Timestamp,
-                            Open = c.Candle.Open,
-                            Close = c.Candle.Close,
-                            Low = c.Candle.Low,
-                            High = c.Candle.High,
-                            TradingVolume = c.Candle.TradingVolume,
-                            TradingOppositeVolume = c.Candle.TradingOppositeVolume,
-                            LastTradePrice = c.Candle.LastTradePrice
-                        })
-                        .ToArray()
-                });
+                return _publisher.ProduceAsync(@event);
             }
         }
 
         public void Dispose()
         {
-            _legacyPublisher?.Dispose();
             _publisher?.Dispose();
         }
 
         public void Stop()
         {
-            _legacyPublisher?.Stop();
             _publisher?.Stop();
         }
     }
