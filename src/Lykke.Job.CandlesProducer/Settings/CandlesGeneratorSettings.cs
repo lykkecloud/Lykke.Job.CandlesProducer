9b9137a92bcc84ff02d4e6fcdffe3d8d6aae6719
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-03-29 08:43:10 +0300

    LWDEV-6571 Fix 2 after CR.

--- src/Lykke.Job.CandlesProducer/Settings/CandlesGeneratorSettings.cs
+++ src/Lykke.Job.CandlesProducer/Settings/CandlesGeneratorSettings.cs
@@ -4,7 +4,6 @@ namespace Lykke.Job.CandlesProducer.Settings
 {
     public class CandlesGeneratorSettings
     {
-        public TimeSpan MinCacheAge { get; set; }
         public TimeSpan OldDataWarningTimeout { get; set; }
     }
 }
