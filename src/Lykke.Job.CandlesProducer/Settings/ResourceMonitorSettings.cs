1a7c537728461f3fdcfc7be68d8474d40dc2a86a
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-04-02 17:27:16 +0300

    LWDEV-6613 Implemented. Works locally.

---
+++ src/Lykke.Job.CandlesProducer/Settings/ResourceMonitorSettings.cs
@@ -0,0 +1,20 @@
+﻿using Lykke.SettingsReader.Attributes;
+
+namespace Lykke.Job.CandlesProducer.Settings
+{
+    public class ResourceMonitorSettings
+    {
+        public ResourceMonitorMode MonitorMode { get; set; }
+        [Optional]
+        public double CpuThreshold { get; set; }
+        [Optional]
+        public int RamThreshold { get; set; }
+    }
+
+    public enum ResourceMonitorMode
+    {
+        Off,
+        AppInsightsOnly,
+        AppInsightsWithLog
+    }
+}
